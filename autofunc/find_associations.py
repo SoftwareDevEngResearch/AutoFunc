d1d49d12a9cd9f1bded198b7e14ccc11a096a61d
AlexMikes <alexmikes@gmail.com>
2019-05-09 08:13:37 -0700

    Adding setup files for pip install

--- autofunc/find_associations.py
+++ autofunc/find_associations.py
@@ -8,29 +8,8 @@ function, and flow in the repository data.
 
 """
 
-def count_stuff():
 
-    script_dir = os.path.dirname(__file__)
-    file = os.path.join(script_dir, '../assets/bladeQueryClean.csv')
 
-    store_data, records = get_data(file)
 
-    comps = {}
-
-    comp =
-
-    if comp not in comps:
-
-        # if i == sys.argv[1]:
-        #     set1.add(word)
-        # if i == sys.argv[2]:
-        #     set2.add(word)
-
-        # counting[looper] += 1
-
-        comps[comp] = 1
-    else:
-        # repeating[looper] += 1
-        comps[comp] += 1
 
 # if __name__ == "__main__":
